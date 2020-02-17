48c577282c82b437a16cc23ec1fdbf823835c174
igor <ikreknin@bk.ru>
2020-02-17 14:03:20 +0300

    Небольшие дополнения модели поезда

--- Train/Train.hpp
+++ Train/Train.hpp
@@ -3,9 +3,25 @@
 
 class Train{
 private:
-    
+    TrainCar* first;
+    TrainCar* last;    
 
 public:
+	Train(int len){
+		TrainCar* locomotive = new TrainCar(true);
+		this->first = locomotive;
+		this->last = locomotive;
 
+		for (int i = 0; i < len; i++){
+			TrainCar* car = new TrainCar(false);
+			this->last->setNext(car);
+			this->last = car;
+		}
+	}
+
+	void addCar(TrainCar* ) return;
+	void removeCar(int id) return;
+
+	void replaceTrain() return;
 
 }
