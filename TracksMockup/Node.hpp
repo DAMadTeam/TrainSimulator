b828c40024a51b0295584ac5bcc3e3ca6c84ba65
DarthPointer <al.kiriackin@yandex.ru>
2020-02-21 13:30:41 +0300

    Исправил недоделки в Node

--- TracksMockup/Node.hpp
+++ TracksMockup/Node.hpp
@@ -16,7 +16,7 @@ public:
 
     int AddTrackSeg(TrackSeg*, bool);
     void RemoveSeg(unsigned char);
-    void shiftCart(TrackSeg*&, unsigned char);
+    void shiftCart(TrackSeg*&, bool&, unsigned char);
 };
 
 Node::Node(TrackSeg* track)                             //изменить
@@ -30,26 +30,34 @@ Node::Node(TrackSeg* track)                             //изменить
 
 void Node::shiftCart(TrackSeg *&new_seg, bool &direction, unsigned char source_seg_id)
 {
-    if((state >> 6) == 0)                                       //тупик 00X-----
+    if((state >> 6) == 0)                                       //тупик 00D- ----
     {
         throw RRT_DeadEnd_OverRun();
     }
-    else if((state >> 6) == 1)                                  //сквозной узел
+    else if((state >> 6) == 1)                                  //сквозной узел 01DD ----
     {
-        char i = 0;
-        while(i != -1)
+        unsigned char i = 0;
+        unsigned char n = 0;
+        while(i != 255)
         {
             if(i == 4)
             {
-                i = -1;
                 new_seg = 0;
                 throw RRT_Bad_TransferNode();
             }
-            else if((i != source_seg_id) * (tracks[i]))
+            if(tracks[i])
             {
-                new_seg = tracks[i];
-                direction = state & 32;                         //32 = 00100000b
-                i = -1;
+                if(i == source_seg_id)
+                {
+                    i++;
+                    n++;
+                }
+                else
+                {
+                    new_seg = tracks[i];
+                    direction = state & (32 >> n);              //32 = 0010 0000b
+                    i = 255;                                    //     01DD ----
+                }
             }
             else
             {
@@ -57,8 +65,137 @@ void Node::shiftCart(TrackSeg *&new_seg, bool &direction, unsigned char source_s
             }
         }
     }
-    else if((state >> 6) == 2)                                  //стрелка
+    else if((state >> 6) == 2)                                  //стрелка 10DD DRRS
     {
-
+        unsigned char root_id = (state & 6) >> 2;               //6 = 0000 0110b
+        if(source_seg_id == root_id)
+        {
+            unsigned char i = 0;
+            unsigned char n = 0;
+            bool pass_next_branch = state & 1;                  //1 = 0000 0001b
+            while(i != 255)
+            {
+                if(i == 4)
+                {
+                    new_seg = 0;
+                    throw RRT_Bad_Switch();
+                }
+                else if(tracks[i])
+                {
+                    if(i == root_id)
+                    {
+                        i++;
+                        n++;
+                    }
+                    else if(pass_next_branch)
+                    {
+                        i++;
+                        n++;
+                        pass_next_branch = 0;
+                    }
+                    else
+                    {
+                        new_seg = tracks[i];
+                        direction = state & (32 >> n);          //32 = 0010 0000b
+                        i = 255;                                //     10DD DRRS
+                    }
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
+        else //if(source_seg_id != root_id)
+        {
+            bool source_is_second_branch = (source_seg_id == 3) + (source_seg_id == 2) * ((root_id == 3) + (tracks[3] == 0)) + 
+                            (source_seg_id == 1) * (tracks[0] != 0) * (root_id != 0);
+            unsigned char i = 0;
+            unsigned char n = 0;
+            while(i != 255)
+            {
+                if(i == 4)
+                {
+                    new_seg = 0;
+                    throw RRT_Bad_Switch();
+                }
+                else if(tracks[i])
+                {
+                    if(i == root_id)
+                    {
+                        i == 255;
+                    }
+                    else
+                    {
+                        i++;
+                        n++;
+                    }
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            new_seg = tracks[root_id];
+            direction = state & (32 >> n);
+            if(source_is_second_branch != (state & 1))          //переделать - нехер исключением передавать чатсь допустимых* исходов
+            {
+                throw RRT_Bad_Switch_Branch();
+            }
+        }
+    }
+    else //if ((state >> 6) == 3)                               //перекрёсток 11DD DDCC
+    {
+        unsigned char n;
+        if(source_seg_id == 0)
+        {
+            n = state & 3;
+        }
+        else if(source_seg_id == (state & 3))
+        {
+            n = 0;
+        }
+        else if(source_seg_id == 1)
+        {
+            if((state & 3) == 2)
+            {
+                n = 3;
+            }
+            else //if((state & 3) == 3)
+            {
+                n = 2;
+            }
+        }
+        else if(source_seg_id == 2)
+        {
+            if((state & 3) == 1)
+            {
+                n = 3;
+            }
+            else //if((state & 3) == 3)
+            {
+                n = 1;
+            }
+        }
+        else //if(source_seg_id == 3)
+        {
+            if((state & 3) == 1)
+            {
+                n = 2;
+            }
+            else //if((state & 3) == 2)
+            {
+                n = 1;
+            }
+        }
+        new_seg = tracks[n];
+        if(new_seg)
+        {
+            direction = state & (32 >> n);
+        }
+        else
+        {
+            throw RRT_Bad_Crossing();
+        } 
     }
 }
--- TracksMockup/TrackExceptions.h
+++ TracksMockup/TrackExceptions.h
@@ -8,8 +8,32 @@ class RRT_DeadEnd_OverRun : public std::exception
 
 class RRT_Bad_TransferNode : public std::exception
 {
-    const chat *what() const noexcept override
+    const char *what() const noexcept override
     {
         return "002";
+    }
+};
+
+class RRT_Bad_Switch : public std::exception
+{
+    const char *what() const noexcept override
+    {
+        return "003";
+    }
+};
+
+class RRT_Bad_Switch_Branch : public std::exception
+{
+    const char *what() const noexcept override
+    {
+        return "004";
+    }
+};
+
+class RRT_Bad_Crossing : public std::exception
+{
+    const char *what() const noexcept override
+    {
+        return "005";
     }
 };
