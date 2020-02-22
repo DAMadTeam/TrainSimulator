d8bda0f3bc79242ea543985c9d4d44f86d614408
DarthPointer <al.kiriackin@yandex.ru>
2020-02-22 22:58:53 +0300

    подцепил Node::shiftCart к TrackSeg::shiftCart

--- TracksMockup/TrackSeg.hpp
+++ TracksMockup/TrackSeg.hpp
@@ -11,7 +11,7 @@ class TrackSeg
 private:
     TrackShape *shape;
     Node *fwd, *bck;
-    unsigned char fwd_id, bck_idl;
+    unsigned char fwd_id, bck_id;
 
 public:
     TrackSeg() = delete;
@@ -26,5 +26,12 @@ public:
 
 void TrackSeg::ShiftCart(TrackSeg *&next_seg, bool &direction)
 {
-    
+    if(direction)
+    {
+        bck->shiftCart(next_seg, direction, bck_id);
+    }
+    else
+    {
+        fwd->shiftCart(next_seg, direction, fwd_id);
+    }
 }
